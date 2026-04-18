095b415750c5fa254289b3a8e8b81ba4cd6c33bb
SiLeader <sileader.dev@gmail.com>
2026-04-18 15:41:25 +0900

    update config

---
+++ test-scripts/embeddings.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+curl http://localhost:8080/v1/embeddings \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "input": "What is the capital of Tokyo?",
+    "model": "ruri-v3-310m",
+    "encoding_format": "float"
+  }'
